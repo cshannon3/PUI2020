f7cb42d14c3e19727e11091f50e05605363c7ca0
cshannon3 <conreshan@gmail.com>
2020-10-10 03:29:59 -0400

    added logo icon

--- text.js
+++
@@ -1,6 +0,0 @@
-let homeText ={
-    "title":"Muddy Paws Adventure Gear",
-    "intro":"The mecca for outdoor pet gear. So high quality that switching from us to our competitors is considered animal cruelty.",
-    "sub-intro":"We are a small outdoor pet equiptment manufacturing company who is obsessed with quality. We cater to pet owners who know their pet deserve the very best.",
-    "body": "We opened our doors way back in 1862, manufacturing Cat Backpacks for Union Generals who wanted to bring their cats with them into battle. From these humble beginnings, we’ve expanded our offerings six-fold, to six amazing products including a cat harness, and gps tracker. \n\n Our products stand the test of time. "
-};
